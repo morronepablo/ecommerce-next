758b3a621a56ee25132e90558e8b994fc2009d6e
morronepablo <morronepablo@gmail.com>
2021-05-20 11:07:59 -0300

    Actualizacion de contraseña

--- client/components/Account/ChangePasswordForm/ChangePasswordForm.js
+++ client/components/Account/ChangePasswordForm/ChangePasswordForm.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from "semantic-ui-react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
+import { updatePasswordApi } from "../../../api/user";
 
 export default function ChangePasswordForm(props) {
     const { user, logout } = props;
+    const [loading, setLoading] = useState(false);
 
     const formik = useFormik({
         initialValues: initialValues(),
         validationSchema: Yup.object(validationSchema()),
-        onSubmit: (formData) => {
-        console.log("formData ", formData);
-
+        onSubmit: async (formData) => {
+        setLoading(true);
+        const response = await updatePasswordApi(user.id, formData.password, logout);
+        if(!response) {
+            toast.error("Error al actualizar la contraseña");
+        } else {
+            logout();
+        }
+        setLoading(false);
         },
     });
 
@@ -24,7 +32,7 @@ export default function ChangePasswordForm(props) {
                     <Form.Input name="password" type="password" placeholder="Tu nueva contraseña" onChange={formik.handleChange} value={formik.values.password} error={formik.errors.password} />
                     <Form.Input name="repeatPassword" type="password" placeholder="Confirma tu nueva contraseña" onChange={formik.handleChange} value={formik.values.repeatPassword} error={formik.errors.repeatPassword} />
                 </Form.Group>
-                <Button className="submit">Actualizar</Button>
+                <Button className="submit" loading={loading}>Actualizar</Button>
             </Form>
         </div>
     );
