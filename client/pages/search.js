f08b20abd3a8c6306a14d83bd9894e996e9ca7d4
morronepablo <morronepablo@gmail.com>
2021-05-25 14:19:11 -0300

    Obteniendo los resultados de la búsqueda

--- client/pages/search.js
+++ client/pages/search.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import BasicLayout from "../layouts/BasicLayout";
+import { useRouter } from "next/router";
+import { searchGamesApi } from "../api/game";
+import { size } from "lodash";
 
 export default function search() {
+    const [games, setGames] = useState(null);
+    const { query } = useRouter();
+    
 
     useEffect(() => {
         document.getElementById("search-game").focus();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            if(size(query.query) > 0){
+                const response = await searchGamesApi(query.query);
+                if(size(response) > 0) setGames(response);
+                else setGames([]);
+            } else {
+                setGames([]);
+            }
+        })()
+    }, [query])
+
+
     return (
         <BasicLayout className="search">
             <h1>Busqueda...</h1>
