caf074b3c9ded57ffbdf13cbadbd935734ce064c
morronepablo <morronepablo@gmail.com>
2021-05-24 22:34:36 -0300

    Creando el carrousel de imágenes

--- client/components/Game/InfoGame/InfoGame.js
+++ client/components/Game/InfoGame/InfoGame.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactPlayer from 'react-player/lazy';
+import CarouselScreenshots from "../CarouselScreenshots"
 
 export default function InfoGame(props) {
     const { game } = props;
 
     return (
         <div className="info-game">
-            <ReactPlayer className="info-game__video" url={game.video} controls={true} />
+            <ReactPlayer 
+                className="info-game__video" 
+                url={game.video} 
+                controls={true} 
+            />
+            <CarouselScreenshots title={game.title} screenshots={game.screenshots} />
         </div>
     )
 }
