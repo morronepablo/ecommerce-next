6416b35ca395d6700099e3e2202d8f9ed97b9e2a
morronepablo <morronepablo@gmail.com>
2021-05-25 12:18:33 -0300

    Eliminar juegos de favoritos

--- client/components/Game/HeaderGame/HeaderGame.js
+++ client/components/Game/HeaderGame/HeaderGame.js
@@ -3,7 +3,8 @@ import { Grid, Image, Icon, Button, GridColumn } from "semantic-ui-react";
 import { size } from "lodash";
 import classNames from "classnames";
 import useAuth from "../../../hooks/useAuth";
-import { isFavoriteApi, addFavoriteApi } from "../../../api/favorite";
+import { isFavoriteApi, addFavoriteApi, deleteFavoriteApi } from "../../../api/favorite";
+import { toast } from 'react-toastify';
 
 export default function HeaderGame(props) {
     const { game } = props;
@@ -26,6 +27,7 @@ function Info(props) {
     const { title, summary, price, discount } = game;
     const [isFavorite, setIsFavorite] = useState(false);
     const [reloadFavorite, setReloadFavorite] = useState(false);
+    const [loading, setLoading] = useState(false);
     const {auth, logout} = useAuth();
 
 
@@ -42,15 +44,22 @@ function Info(props) {
 
     const addFavorite = async () => {
         if(auth) {
+            toast.warning("Agregando a lista de favoritos");
             await addFavoriteApi(auth.idUser, game.id, logout);
             setReloadFavorite(true);
         } else {
-            console.log("necesita loguearse");
+            toast.warning("Necesita loguearse para agregar a favoritos");
         }
     }
 
-    const deleteFavorite = () => {
-        console.log("Eliminar de favoritos");
+    const deleteFavorite = async () => {
+        if(auth) {
+            toast.warning("Eliminando de la lista de favoritos");
+            await deleteFavoriteApi(auth.idUser, game.id, logout);
+            setReloadFavorite(true);
+        } else {
+            toast.warning("Necesita loguearse para eliminar de favoritos");
+        }
     }
 
     return (
