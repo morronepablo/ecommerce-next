b625d34427c960499712f62b82d31c8e5b36fd17
morronepablo <morronepablo@gmail.com>
2021-05-24 23:20:01 -0300

    Fecha de lanzamiento y descripción

--- client/components/Game/InfoGame/InfoGame.js
+++ client/components/Game/InfoGame/InfoGame.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactPlayer from 'react-player/lazy';
+import moment from "moment";
+import "moment/locale/es";
 import CarouselScreenshots from "../CarouselScreenshots"
 
 export default function InfoGame(props) {
@@ -13,6 +15,13 @@ export default function InfoGame(props) {
                 controls={true} 
             />
             <CarouselScreenshots title={game.title} screenshots={game.screenshots} />
+            <div className="info-game__content">
+                <div dangerouslySetInnerHTML={{__html: game.summary}} />
+                <div className="info-game__content-date">
+                    <h4>Fecha de lanzamiento:</h4>
+                    <p>{moment(game.releaseDate).format("LL")}</p>
+                </div>
+            </div>
         </div>
     )
 }
