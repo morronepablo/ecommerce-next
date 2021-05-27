bdbb51986e388cb749e1043022c3c9cdadc84940
morronepablo <morronepablo@gmail.com>
2021-05-27 13:56:38 -0300

    Creando la página del carrito

---
+++ client/pages/cart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BasicLayout from "../layouts/BasicLayout";
+
+export default function Cart() {
+    return (
+        <BasicLayout className="cart">
+            <h1>Carrito</h1>
+        </BasicLayout>
+    )
+}
