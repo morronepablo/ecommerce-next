193093fc8752ab8a3a5a77e03ce48384dafe94a1
morronepablo <morronepablo@gmail.com>
2021-05-28 18:41:40 -0300

    Dependencias de Stripe para el cliente

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,3 +1,4 @@
 export const BASE_PATH = "http://localhost:1337";
 export const TOKEN = "token";
 export const CART = "cart";
+export const STRIPE_TOKEN = "pk_test_51IwCe3L6LVvPXi7rdCP01KHQ8mJ9CICn5SGreziNrstr1qlRtw3vRjupgueNw6uiG7GmSfGuH38bVDjCg5VHw4Hj00YHwqljQJ";
