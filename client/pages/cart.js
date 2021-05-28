588c4114872b23b2b7f7681c276a20a903f17c1c
morronepablo <morronepablo@gmail.com>
2021-05-28 14:55:16 -0300

    Seleccionando la dirección de envío

--- client/pages/cart.js
+++ client/pages/cart.js
@@ -24,7 +24,7 @@ function FullCart(props) {
     const { products } = props;
     const [productsData, setProductsData] = useState(null);
     const [reloadCart, setReloadCart] = useState(false);
-    
+    const [address, setAddress] = useState(null);
     
     useEffect(() => {
         (async () => {
@@ -45,7 +45,7 @@ function FullCart(props) {
                 reloadCart={reloadCart} 
                 setReloadCart={setReloadCart}
             />
-            <AddressShipping />
+            <AddressShipping setAddress={setAddress} />
         </BasicLayout>
     );
 }
